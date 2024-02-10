4a1d2ccf7fb5f5ca9c10cfd1de91ec0a4db0bbee
Youngbin Han <ybhan@ubuntu.com>
2024-02-11 01:47:43 +0900

    Footer 수정

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,10 +8,16 @@ export const languages = {
   export const ui = {
     en: {
       'home.tagline': 'The second edition of UbuCon Korea to be held at Microsoft Korea which is the same venue as last year on August 10th. Various sessions will be prepared with theme "Deploy with Ubuntu"!',
-      'home.venuetbd': '*Dates and venue to be confirmed soon.'
+      'home.venuetbd': '*Dates and venue to be confirmed soon.',
+      'footer.contact': 'Contact us with Email',
+      'footer.srcrepo': 'See source code',
+      'footer.otherevents': 'Other events of Ubuntu Korea',
     },
     ko: {
       'home.tagline': '두번째 UbuCon Korea는 작년과 동일한 장소인 한국마이크로소프트에서 8월 10일에 개최될 예정입니다. "Deploy with Ubuntu"를 테마로 다양한 세션이 준비 될 예정입니다.',
-      'home.venuetbd': '*장소 및 날짜 추후 확정 예정.'
+      'home.venuetbd': '*장소 및 날짜 추후 확정 예정.',
+      'footer.contact': '이메일로 연락하기',
+      'footer.srcrepo': '소스코드 확인',
+      'footer.otherevents': '우분투한국커뮤니티의 다른 행사',
     },
   } as const;
