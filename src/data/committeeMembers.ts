b7f54f6a6a8bc92abe7895e8c0e82faede525966
paperbox <lego37yoon@outlook.com>
2024-03-03 01:40:17 +0900

    운영진 목록 추가
    - 운영진 기준은 Discord 내 언급 및 실제 Discord 사용자 목록에서 UCK24-committee로 분류된 분들을 기준으로 하였습니다.

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -1,9 +1,105 @@
 
 export const committeeMembers = [
     {
+        name: "Junsang Mun",
+        team: "Ubuntu Korea Community",
+        role: "General, Contents, Venue and Safety, Social Media and Design",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/junmoon/144/12427_2.png"
+    },
+    {
         name: "Youngbin Han",
         team: "Ubuntu Korea Community, Ubuntu LoCo Council",
         role: "Sponsorship & Financing",
         profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
+    },
+    {
+        name: "Giyeon Bang",
+        team: "C++ Korea",
+        role: "General, Venue and Safety, Web and Video, Sponsorship and Finances",
+        profileImageUrl: "https://2022.ubucon.asia/about/giyeon_bang.jpg"
+    },
+    {
+        name: "Sangkon Han",
+        team: "Ubuntu Korea Community",
+        role: "Contents, Venue and Safety",
+        profileImageUrl: "https://2022.ubucon.asia/about/sangkon_han.jpg"
+    },
+    {
+        name: "Minseong Cho",
+        team: "Ubuntu Korea Community",
+        role: "Venue and Safety, Social Media and Design",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/52643858?v=4"
+    },
+    {
+        name: "Yeonguk Choo",
+        team: "Ubuntu Korea Community",
+        role: "General, Contents, Sponsorship and Finances",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/choo121600/144/12050_2.png"
+    },
+    {
+        name: "Minwook Shin",
+        team: "Ubuntu Korea Community",
+        role: "General",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/12551635?v=4"
+    },
+    {
+        name: "JunHyeon Bae",
+        team: "Ubuntu Korea Community",
+        role: "General, Venue and Safety",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/22819926?v=4"
+    },
+    {
+        name: "Gyuseok Jung",
+        team: "Ubuntu Korea Community",
+        role: "Venue and Safety, Web and Video ",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/9061758?v=4"
+    },
+    {
+        name: "Hoehyeong Jung",
+        team: "Ubuntu Korea Community",
+        role: "General, Contents, Sponsorship and Finances",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/hothead-jung/144/12047_2.png"
+    },
+    {
+        name: "Yeongchan Kim",
+        team: "Ubuntu Korea Community",
+        role: "General",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/draco/144/12042_2.png"
+    },
+    {
+        name: "Seowon Jung",
+        team: "Ubuntu Korea Community",
+        role: "General",
+        profileImageUrl: "https://kudos.coe.hawaii.edu/attachments/kapa/persons/photos/000/000/256/original.jpg?1587497520"
+    },
+    {
+        name: "Nayeon Keum",
+        team: "Ubuntu Korea Community",
+        role: "General",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/r/58956e/144.png"
+    },
+    {
+        name: "Taeyong Lee",
+        team: "Ubuntu Korea Community",
+        role: "General, Venue and Safety",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/t/c37758/144.png"
+    },
+    {
+        name: "Yonghyun Yi",
+        team: "Ubuntu Korea Community",
+        role: "General, Venue and Safety",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/d/a88e4f/144.png"
+    },
+    {
+        name: "Soyeon Kwon",
+        team: "Ubuntu Korea Community",
+        role: "General, Venue and Safety",
+        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/a/779978/144.png"
+    },
+    {
+        name: "Jungmin Yoon",
+        team: "Ubuntu Korea Community",
+        role: "Web and Video, Venue and Safety",
+        profileImageUrl: "https://avatars.githubusercontent.com/u/7126454?v=4"
     }
 ]
