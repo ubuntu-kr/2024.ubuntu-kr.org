c2558de9912f14a03d84a7477f3a4c5d28a150ab
paperbox <lego37yoon@outlook.com>
2024-03-03 22:29:04 +0900

    미확정 명단 제거

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -37,12 +37,6 @@ export const committeeMembers = [
         profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/choo121600/144/12050_2.png"
     },
     {
-        name: "Minwook Shin",
-        team: "Ubuntu Korea Community",
-        role: "General",
-        profileImageUrl: "https://avatars.githubusercontent.com/u/12551635?v=4"
-    },
-    {
         name: "JunHyeon Bae",
         team: "Ubuntu Korea Community",
         role: "General, Venue and Safety",
@@ -59,42 +53,6 @@ export const committeeMembers = [
         team: "Ubuntu Korea Community",
         role: "General, Contents, Sponsorship and Finances",
         profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/hothead-jung/144/12047_2.png"
-    },
-    {
-        name: "Yeongchan Kim",
-        team: "Ubuntu Korea Community",
-        role: "General",
-        profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/draco/144/12042_2.png"
-    },
-    {
-        name: "Seowon Jung",
-        team: "Ubuntu Korea Community",
-        role: "General",
-        profileImageUrl: "https://kudos.coe.hawaii.edu/attachments/kapa/persons/photos/000/000/256/original.jpg?1587497520"
-    },
-    {
-        name: "Nayeon Keum",
-        team: "Ubuntu Korea Community",
-        role: "General",
-        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/r/58956e/144.png"
-    },
-    {
-        name: "Taeyong Lee",
-        team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
-        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/t/c37758/144.png"
-    },
-    {
-        name: "Yonghyun Yi",
-        team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
-        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/d/a88e4f/144.png"
-    },
-    {
-        name: "Soyeon Kwon",
-        team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
-        profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/a/779978/144.png"
     },
     {
         name: "Jungmin Yoon",
