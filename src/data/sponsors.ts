9c6920c37def409b5faaa510fb77da2ffb813e6b
paperbox <lego37yoon@outlook.com>
2024-04-26 09:10:25 +0900

    후원사 추가

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,5 +1,6 @@
 import type { ColSize } from "@canonical/react-components"
 import logoPlaceHolder from "@assets/logo_placeholder.svg";
+import groMetric from "@assets/sponsors/grometric.svg";
 
 export const sponsors = [
     {
@@ -57,10 +58,10 @@ export const sponsors = [
         sizeOnSmallScreen: 1 as ColSize,
         list: [
             {
-                name: "Become a sponsor!",
-                logoUrl: logoPlaceHolder.src,
-                url: "/sponsors/become-a-sponsor",
-                description: "This sponsor slot is currently empty and available! Would like to support our event and become our sponsor? Click 'Visit website' button to join us today!"
+                name: "GroMetric",
+                logoUrl: groMetric.src,
+                url: "http://www.grometric.kr/",
+                description: "그로메트릭은 고객의 지속가능한 성장을 위한 인텔리전스와 안전함을 제공합니다. 중장기적인 관점에서 데이터 메트릭을 통한 고객의 성장을 돕는 솔루션 개발을 목표하고 있어요. 아울러 다양한 OpenSource Software, Cloud, Security 솔루션으로 여러분의 성장과 함께 하고 싶어요."
             },
             {
                 name: "Become a sponsor!",
