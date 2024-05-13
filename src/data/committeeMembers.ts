a2e217f86386454686789b3786a2835c98153ec5
paperbox <lego37yoon@outlook.com>
2024-05-13 15:53:45 +0900

    조직위원회 역할 수정

--- src/data/committeeMembers.ts
+++ src/data/committeeMembers.ts
@@ -3,85 +3,85 @@ export const committeeMembers = [
     {
         name: "Junsang Mun",
         team: "Ubuntu Korea Community",
-        role: "General, Contents, Venue and Safety, Social Media and Design",
+        role: "General, Venue and Safety, Social Media and Design",
         profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/junmoon/144/12427_2.png"
     },
     {
         name: "Youngbin Han",
         team: "Ubuntu Korea Community, Ubuntu LoCo Council",
-        role: "Sponsorship & Financing",
+        role: "General, Sponsorship & Financing",
         profileImageUrl: "https://avatars.githubusercontent.com/u/1916739?v=4",
     },
     {
         name: "Giyeon Bang",
         team: "C++ Korea",
-        role: "General, Venue and Safety, Web and Video, Sponsorship and Finances",
+        role: "General, Venue and Safety, Web and Video",
         profileImageUrl: "https://2022.ubucon.asia/about/giyeon_bang.jpg"
     },
     {
         name: "Sangkon Han",
         team: "Ubuntu Korea Community",
-        role: "Contents, Venue and Safety",
+        role: "Contents",
         profileImageUrl: "https://2022.ubucon.asia/about/sangkon_han.jpg"
     },
     {
         name: "Minseong Cho",
         team: "Ubuntu Korea Community",
-        role: "Venue and Safety, Social Media and Design",
+        role: "Contents, Venue and Safety",
         profileImageUrl: "https://avatars.githubusercontent.com/u/52643858?v=4"
     },
     {
         name: "Yeonguk Choo",
         team: "Ubuntu Korea Community",
-        role: "General, Contents, Sponsorship and Finances",
+        role: "Sponsorship and Finances, Venue and Safety",
         profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/choo121600/144/12050_2.png"
     },
     {
         name: "JunHyeon Bae",
         team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
+        role: "Contents, Sponsorship and Finances, Venue and Safety",
         profileImageUrl: "https://avatars.githubusercontent.com/u/22819926?v=4"
     },
     {
         name: "Gyuseok Jung",
         team: "Ubuntu Korea Community",
-        role: "Venue and Safety, Web and Video ",
+        role: "General",
         profileImageUrl: "https://avatars.githubusercontent.com/u/9061758?v=4"
     },
     {
         name: "Hoehyeong Jung",
         team: "Ubuntu Korea Community",
-        role: "General, Contents, Sponsorship and Finances",
+        role: "Web and Video",
         profileImageUrl: "https://discourse.ubuntu-kr.org/user_avatar/discourse.ubuntu-kr.org/hothead-jung/144/12047_2.png"
     },
     {
         name: "Nayeon Keum",
         team: "Ubuntu Korea Community",
-        role: "General",
+        role: "Contents",
         profileImageUrl: "/avatars/nayeon.jpg"
     },
     {
         name: "Tae Yong Lee",
         team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
+        role: "Contents, Sponsorship and Finances, Venue and Safety",
         profileImageUrl: "/avatars/taeyong.jpg"
     },
     {
         name: "Yonghyun Yi",
         team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
+        role: "Contents, Social Media and Design, Venue and Safety",
         profileImageUrl: "https://discourse.ubuntu-kr.org/letter_avatar_proxy/v4/letter/d/a88e4f/144.png"
     },
     {
         name: "Soyeon Kwon",
         team: "Ubuntu Korea Community",
-        role: "General, Venue and Safety",
+        role: "Web and Video, Social Media and Design",
         profileImageUrl: "/avatars/soyeon.png"
     },
     {
         name: "Jungmin Yoon",
         team: "Ubuntu Korea Community",
-        role: "Web and Video, Venue and Safety",
+        role: "Web and Video, Social Media and Design, Venue and Safety",
         profileImageUrl: "https://avatars.githubusercontent.com/u/7126454?v=4"
     }
 ]
