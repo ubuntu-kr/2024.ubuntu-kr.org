96bf93b5e5ec26e944efc7b843ce49539f08f89f
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-06-19 03:07:45 +0900

    모든 연사자 표시

--- src/config.ts
+++ src/config.ts
@@ -18,11 +18,8 @@ export const config = {
     },
     featuredSpeakers: {
         indicoExportUrl: "https://events.canonical.com/export/event/48.json?detail=contributions&occ=yes&pretty=yes",
-        contributions: {
-            7: { profileImg: 1624},
-            21: { profileImg: 2692},
-            10: { profileImg: 2658}
-        }
+        contributionIds: [7, 21, 10],
+        speakerIds: [{ db_id: 562, user: 1624 }, { db_id: 525, user: 2692 }, { db_id: 524, user: 2699 }, { db_id: 526, user: 2658 }]
     },
     cityBanner: {
         title: "cityBanner.title",
