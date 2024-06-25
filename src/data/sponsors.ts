c2445eee0ee2243dfb46fe548b60eb59ede386c6
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-06-25 22:08:51 +0900

    후원사 추가 [한국마이크로소프트]

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,5 +1,6 @@
 import type { ColSize } from "@canonical/react-components"
 import logoPlaceHolder from "@assets/logo_placeholder.svg";
+import microsoft from "@assets/sponsors/microsoft.png";
 import groMetric from "@assets/sponsors/grometric.svg";
 import ahnlabCloudmate from "@assets/sponsors/ahnlab_cloudmate.png";
 
@@ -15,6 +16,14 @@ export const sponsors = [
         sizeOnLargeScreen: 4 as ColSize,
         sizeOnMediumScreen: 4 as ColSize,
         sizeOnSmallScreen: 3 as ColSize,
+        list: [
+            {
+                name: "Microsoft Korea",
+                logoUrl: microsoft.src,
+                url: "https://azure.microsoft.com/en-us/ubuntu/?ocid=AID3035196",
+                description: "Microsoft는 지구상의 모든 사람과 조직이 더 많은 것을 성취할 수 있도록 역량을 지원하는 것을 사명으로 삼고 있습니다. Ubuntu 기반 워크로드는 Azure에서 가장 빠르게 성장하고 있는 워크로드 중 하나입니다. Microsoft는 Canonical과 제휴하여 Azure에서 동작하는 Ubuntu와 오픈 소스 커뮤니티 참여를 통해 Azure에 최적화된 커널, 개선된 성능, 고급 보안, 자동 패치 및 업데이트, 유연한 가격 책정 옵션을 제공합니다."
+            }
+        ]
     },
     {
         sponsorLevelName: "Gold",
