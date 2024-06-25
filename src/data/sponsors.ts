dc0e418ef90abfb53a9a1b099c43808490e8876f
paperbox <7126454+lego37yoon@users.noreply.github.com>
2024-06-25 22:11:53 +0900

    후원사 추가 [Canonical Ubuntu]

--- src/data/sponsors.ts
+++ src/data/sponsors.ts
@@ -1,8 +1,9 @@
 import type { ColSize } from "@canonical/react-components"
 import logoPlaceHolder from "@assets/logo_placeholder.svg";
+import ubuntu from "@assets/sponsors/ubuntu.svg";
 import microsoft from "@assets/sponsors/microsoft.png";
-import groMetric from "@assets/sponsors/grometric.svg";
 import ahnlabCloudmate from "@assets/sponsors/ahnlab_cloudmate.png";
+import groMetric from "@assets/sponsors/grometric.svg";
 
 export const sponsors = [
     {
@@ -10,6 +11,14 @@ export const sponsors = [
         sizeOnLargeScreen: 6 as ColSize,
         sizeOnMediumScreen: 6 as ColSize,
         sizeOnSmallScreen: 6 as ColSize,
+        list: [
+            {
+                name: "Ubuntu",
+                logoUrl: ubuntu.src,
+                url: "https://ubuntu.com/",
+                description: "Canonical은 대부분의 퍼블릭 클라우드 워크로드를 위한 OS이자 스마트 게이트웨이, 자율 주행 자동차 및 Advanced robot 등 새롭게 떠오르는 분야를 위한 OS인 Ubuntu를 개발 및 배포하는 회사입니다. Canonical은 상용 Ubuntu 사용자에게 엔터프라이즈 보안, 지원 및 서비스를 제공합니다."
+            }
+        ]
     },
     {
         sponsorLevelName: "Venue",
